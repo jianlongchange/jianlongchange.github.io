46e6ca9582a7a3e664c00355902cac6dc5c3f0c8
jianlongchange <chjianlong@126.com>
2025-01-02 07:07:42 +0000

    Deploying to gh-pages from @ jianlongchange/jianlongchange.github.io@fc2535c77eb4492c1523c1842cc018affd1a3b25 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Some important AI solutions.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
